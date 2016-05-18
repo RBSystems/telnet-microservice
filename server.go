a6b3a87e54c313557584f1ac23d525b944f82264
Jesse Millar <hellojessemillar@gmail.com>
2016-05-18 09:21:56 -0600

    Adding HATEOAS information

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	"github.com/byuoitav/hateoas"
 	"github.com/byuoitav/telnet-microservice/controllers"
 	"github.com/jessemillar/health"
 	"github.com/labstack/echo"
@@ -11,10 +12,17 @@ import (
 )
 
 func main() {
+	err := hateoas.Load("https://raw.githubusercontent.com/byuoitav/telnet-microservice/master/swagger.yml")
+	if err != nil {
+		fmt.Printf("Could not load swagger.yaml file. Error: %s", err.Error())
+		panic(err)
+	}
+
 	port := ":8001"
 	e := echo.New()
 	e.Pre(middleware.RemoveTrailingSlash())
 
+	e.Get("/", hateoas.RootResponse)
 	e.Get("/health", health.Check)
 
 	e.Get("/prompt/:address", controllers.GetPrompt)
