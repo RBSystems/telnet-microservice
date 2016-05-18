6218ae411002a2027376a5e453b447d7850fff0b
Jesse Millar <hellojessemillar@gmail.com>
2016-05-18 14:45:24 -0600

    Adding a deploy script for Jenkins

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Pasted into Jenkins to build (will eventually be fleshed out to work with a Docker Hub and Amazon AWS)
+
+echo "Stopping running application"
+docker stop telnet-microservice
+docker rm telnet-microservice
+
+echo "Building container"
+docker build -t byuoitav/telnet-microservice .
+
+echo "Starting the new version"
+docker run -d --restart=always --name telnet-microservice -p 8000:8000 byuoitav/telnet-microservice:latest
